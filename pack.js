c37556094426636f7d95b19a3b836d917bbae8cc
Jason Godesky <jefgodesky@gmail.com>
2026-01-02 15:36:31 -0500

    Unpack is no longer necessary

--- pack.js
+++ pack.js
@@ -1,8 +1,8 @@
 import { compilePack } from '@foundryvtt/foundryvtt-cli'
-import module from '../src/module.json' with { type: 'json' }
+import module from './src/module.json' with { type: 'json' }
 
 for await (const pack of module.packs) {
   const src = `src/${pack.path}`
   const dest = `dist/${pack.path}`
-  await compilePack(src, dest)
+  await compilePack(src, dest, { yaml: true, log: true })
 }
